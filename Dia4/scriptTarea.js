fbd6eb2ebeb793a8f0efe1568b7186f7637c97f1
gabyalvaradob <gaby1237@hotmail.com>
2019-06-30 16:07:12 -0500

    Tarea actualizada

--- Dia4/scriptTarea.js
+++ Dia4/scriptTarea.js
@@ -54,3 +54,36 @@ for (let i = 0; i < platillos.length; i++) {
     }
 }
 console.log(pares);
+
+//Tarea RFC
+
+let nombre = "Karla";
+let apellidoP = "Alvarado";
+let apellidoM = "Barrancos";
+let diaNac = '12';
+let mesNac = '07';
+let yearNac = '90';
+
+lugar = '',    
+limite    = 1,
+primL = apellidoP.split(lugar, limite);
+
+terL = apellidoM.split(lugar, limite);
+
+cuarL = nombre.split(lugar, limite);
+
+let letra = Array.from(apellidoP);
+let vocales = [];
+
+for (let i = 0; i < letra.length; i++) {
+    if (letra[i] === 'A' || letra[i] === 'E' || letra[i] === 'I' || letra[i] === 'O'
+            || letra[i] === 'U' || letra[i] === 'a' || letra[i] === 'e' || letra[i] === 'i'
+            || letra[i] === 'o' || letra[i] === 'u'){
+                vocales.push(letra[i]);
+            }
+}
+console.log(vocales);
+
+let segL = (vocales[0]);
+
+console.log("Su RFC es: " +primL+segL+terL+cuarL+yearNac+mesNac+diaNac);
